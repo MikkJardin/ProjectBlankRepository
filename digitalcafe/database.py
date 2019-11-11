2b687a3060445a063e27b523288fdad6575f03d3
MikkJardin <55426889+MikkJardin@users.noreply.github.com>
2019-11-11 12:50:44 +0800

    Add files via upload

--- digitalcafe/database.py
+++ digitalcafe/database.py
@@ -18,17 +18,41 @@ branches = {
     5: {"name":"Arcovia","phonenumber":"09179990004"}
 }
 
+users = {
+    "chums@example.com":{"password":"Ch@ng3m3!",
+                         "first_name":"Matthew",
+                         "last_name":"Uy"},
+    "joben@example.com":{"password":"Ch@ng3m3!",
+                         "first_name":"Joben",
+                         "last_name":"Ilagan"},
+    "bong@example.com":{"password":"Ch@ng3m3!",
+                        "first_name":"Bong",
+                        "last_name":"Olpoc"},
+    "joaqs@example.com":{"password":"Ch@ng3m3!",
+                         "first_name":"Joaqs",
+                         "last_name":"Gonzales"},
+    "gihoe@example.com":{"password":"Ch@ng3m3!",
+                         "first_name":"Gio",
+                         "last_name":"Hernandez"},
+    "vic@example.com":{"password":"Ch@ng3m3!",
+                       "first_name":"Vic",
+                       "last_name":"Reventar"},
+    "joe@example.com":{"password":"Ch@ng3m3!",
+                       "first_name":"Joe",
+                       "last_name":"Ilagan"},
+}
+
 def get_product(code):
     return products[code]
 
-def get_products(): 
+def get_products():
     product_list = []
-    
+
     for i,v in products.items():
         product = v
         product.setdefault("code",i)
         product_list.append(product)
-        
+
     return product_list
 
 def get_branch(code):
@@ -36,10 +60,16 @@ def get_branch(code):
 
 def get_branches():
     branch_list = []
-    
+
     for i,v in branches.items():
         branch = v
         branch.setdefault("code",i)
         branch_list.append(branch)
-        
+
     return branch_list
+
+def get_user(username):
+    try:
+        return users[username]
+    except KeyError:
+        return None
